76a1fa666b9bb9d151257f29f48f789df5ddb835
cyberplanner <saleh.gfx@gmail.com>
2016-09-05 23:25:54 +0100

    Initial commit.

---
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,15 @@
+package com.fizzbuzz;
+
+public class FizzBuzz {
+
+	public static String getNumber(int i) {
+		if(i % 3 == 0){
+			return "Fizz";
+		}
+		else {
+			return "Buzz";
+		}
+		
+	}
+
+}
---
+++ src/com/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,24 @@
+package com.fizzbuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void test3() {
+		assertTrue(FizzBuzz.getNumber(3).equals("Fizz"));
+	}
+	
+	@Test
+	public void test5() {
+		assertTrue(FizzBuzz.getNumber(5).equals("Buzz"));
+	}
+	
+	@Test
+	public void test6() {
+		assertTrue(FizzBuzz.getNumber(6).equals("Fizz"));
+	}
+
+}
